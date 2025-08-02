99cd47b476d90cc45e0791ff8ea7e1f71d04c83a
SrothJr <nazimhossain14602@gmail.com>
2025-08-03 02:39:56 +0600

    fixining my first bug for this project, many more to come.

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port:", PORT);
 });
