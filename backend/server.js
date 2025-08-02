12d8417292c2ec45cc32ed50a9f423a19e10752f
SrothJr <nazimhossain14602@gmail.com>
2025-08-03 01:50:00 +0600

    Connecting MongoDB Database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 const app = express();
 
+app.get("/", (req, res) => res.send("Hello world123"));
+
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running on port 3000");
 });
