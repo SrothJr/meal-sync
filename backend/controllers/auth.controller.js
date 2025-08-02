d9cd761eb4680c48bf13dbc1ba6dd46fb61673e6
SrothJr <nazimhossain14602@gmail.com>
2025-08-03 05:01:47 +0600

    doing the signup part (without verification)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,49 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
+import { generateVerificationTokenAndSetCookie } from "../utils/generateVerificationTokenAndSetCookie.js";
+
+// SignUp
 export const signup = async (req, res) => {
-  res.send("Sign up controller");
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required!");
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const verificationCode = generateVerificationCode();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verficationToken: verificationCode,
+      verficationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    await user.save();
+    generateVerificationTokenAndSetCookie(res, user._id);
+    //user created and token generated + Cookie set
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
