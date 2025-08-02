60c3e0a110b74ce0c8a0b4321e31c3e248cf4645
SrothJr <nazimhossain14602@gmail.com>
2025-08-03 02:16:00 +0600

    creating routes, controller for authentication and user model

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Sign up controller");
+};
+
+export const login = async (req, res) => {
+  res.send("login controller");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout controller");
+};
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
-
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world123"));
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
