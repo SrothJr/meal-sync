e987658754c0a2fba48f67bfbc598a664c6b51a8
SrothJr <nazimhossain14602@gmail.com>
2025-08-04 21:20:31 +0600

    done with backend for find chefs

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); //parse incoming requests: req.body
 app.use(cookieParser()); // parse incoming cookies
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectDB();
