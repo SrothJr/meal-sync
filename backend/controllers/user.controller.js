aff03e13f31f9ace676abd8e8e4485b0079aefb8
SrothJr <nazimhossain14602@gmail.com>
2025-08-04 23:39:12 +0600

    user controller updated with getUserProfile and updateUserProfile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -23,3 +23,52 @@ export const findChefsInArea = async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.error("Error in getUserProfile: ", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const { name, area } = req.body;
+
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    user.name = name || user.name;
+    user.area = area || user.area;
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      user: {
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        area: updatedUser.area,
+        role: updatedUser.role,
+      },
+    });
+  } catch (error) {
+    console.error("Error in updateUserProfile: ", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
