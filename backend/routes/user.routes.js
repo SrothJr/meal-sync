36acefe98c35229ce9aab7dd8e82af8b978459c3
SrothJr <nazimhossain14602@gmail.com>
2025-08-04 23:41:44 +0600

    user router updated for profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { findChefsInArea } from "../controllers/user.controller.js";
+import {
+  findChefsInArea,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/find-chefs", verifyToken, findChefsInArea);
+
+router.get("/profile", verifyToken, getUserProfile);
+router.put("/profile", verifyToken, updateUserProfile);
 
 export default router;
