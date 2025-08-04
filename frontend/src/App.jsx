a74b3063649b884eefbb7bad91d5e1731b1e46c3
SrothJr <nazimhossain14602@gmail.com>
2025-08-04 23:08:00 +0600

    implementing centering effect to each file instead of the main app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
   console.log("user", user);
   if (isCheckingAuth) return <LoadingSpinner />;
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 relative overflow-hidden">
       <Routes>
         <Route
           path="/"
