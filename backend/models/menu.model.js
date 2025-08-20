70923201c78841361481f3d53366208c2201b6d1
SrothJr <nazimhossain14602@gmail.com>
2025-08-20 19:29:34 +0600

    menu model creation

---
+++ backend/models/menu.model.js
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from "mongoose";
+
+const menuItemSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    required: true,
+    enum: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+  },
+  mealType: {
+    type: String,
+    required: true,
+    enum: ["Breakfast", "Lunch", "Dinner"],
+  },
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true, min: 0 },
+});
+
+const menuSchema = new mongoose.Schema(
+  {
+    chef: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Menu = mongoose.model("Menu", menuSchema);
+export default Menu;
